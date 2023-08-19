c23130954708adf974d5cc811d2bdb6b083336cb
SuperACER <gurbanmyradowmeylis280@gmail.com>
2023-08-19 12:25:06 +0500

    did the localstorage

--- js/app.js
+++ js/app.js
@@ -1,14 +1,66 @@
-let todoList = [
-    {
-      id: 0,
-      todo: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur minus debitis omnis magni optio, atque dolore quas ipsam fugit, quibusdam asperiores? Itaque sunt, excepturi quae impedit maiores ducimus similique tempora?",
-      createdAt: "8.18.2023 / 7 : 5 : 19",
-      updatedAt: "8.18.2023 / 7 : 5 : 19",
-    },
-  ],
+let todoList = [],
   identifier = 1;
 // ! when clickes the button add
 document.getElementById("add").addEventListener("click", () => {
+  adderTodoList(identifier, inputValueRecipient(), getDate(), getDate(), true);
+});
+
+// ! gets input's value and returns it
+function inputValueRecipient() {
+  return document.getElementsByTagName("input").item(0).value;
+}
+
+// ! gets date of current time
+function getDate() {
+  let d = new Date();
+  return `${
+    d.getMonth() + 1
+  }.${d.getDate()}.${d.getFullYear()} / ${d.getSeconds()} : ${d.getMinutes()} : ${d.getHours()}`;
+}
+
+// ! copies the svg
+function svgCopier(editor1, remover1) {
+  const editor = editor1.cloneNode(true);
+  const remover = remover1.cloneNode(true);
+  remover.setAttribute("class", "recycle_bin-primary");
+  editor.setAttribute("class", "editor-primary");
+  return {
+    editor,
+    remover,
+  };
+}
+
+// ! will get the data when site loaded
+function gettingInfo() {
+  if (localStorage.length != 0) {
+    todoList = JSON.parse(localStorage.getItem("todo"));
+    todoList.forEach((item) => {
+      adderTodoList(item.id, item.todo, item.createdAt, item.updatedAt);
+    });
+  }
+}
+// ! will be something before user leave the site
+function savingInfo() {
+  localStorage.setItem("todo", JSON.stringify(todoList));
+}
+
+// ! adds information into the global array (todoList)
+function addTodo(id, todo, createdAt, updatedAt) {
+  todoList.push({
+    id,
+    createdAt,
+    todo,
+    updatedAt,
+  });
+}
+
+function adderTodoList(
+  cardId,
+  cardContent,
+  cardCreatedAt,
+  cardUpdatedAt,
+  isAdd
+) {
   // ! creates elements of card
   let cards = document.getElementById("cards"),
     wrapperOfCard = document.getElementById("wrapperOfCard"),
@@ -37,34 +89,35 @@ document.getElementById("add").addEventListener("click", () => {
   // ! end
 
   // ! sets number card's id
-  id.innerText = `Id : ${identifier}`;
+  id.innerText = `Id : ${cardId}`;
 
   // ! increments the identifier
   identifier++;
 
   // ! sets text to content. text --that in input
-  content.innerText = inputValueRecipient();
+  content.innerText = cardContent;
 
-  // ! pushes info from card into the array
-  addTodo(identifier, content.innerText, getDate(), getDate());
+  if (isAdd) {
+    // ! pushes info from card into the array
+    addTodo(identifier, content.innerText, getDate(), getDate());
+  }
 
   // ! if the card empty
-  if (inputValueRecipient().length == 0) {
+  if (content.length == 0) {
     containerOfContent.className = "break-all flex-auto h-7";
   } else {
     containerOfContent.className = "break-all flex-auto font-normal";
   }
 
   // ! gets date and sets into these element
-  createdAt.innerText = `Created at : ${getDate()}`;
-  updatedAt.innerText = `Updated at : ${getDate()}`;
+  createdAt.innerText = `Created at : ${cardCreatedAt}`;
+  updatedAt.innerText = `Updated at : ${cardUpdatedAt}`;
 
   // ! removes the card of to do
   containerOfRemover.addEventListener("click", () => {
     if (confirm("Are sure to remove")) {
       container.remove();
       let currentId = id.innerText.split(": ")[1];
-      console.log(todoList);
       todoList.splice(
         todoList.findIndex((item, i) => item.id == currentId),
         1
@@ -79,7 +132,7 @@ document.getElementById("add").addEventListener("click", () => {
   containerOfEditor.append(clonedEditor);
   containerOfRemover.append(svgCopier(editor, remover).remover);
 
-  // ! fill the clasname of the input
+  // ! edit section
   let active = false;
   clonedEditor.addEventListener("click", () => {
     if (!active) {
@@ -113,8 +166,7 @@ document.getElementById("add").addEventListener("click", () => {
           }
         );
         updatedAt.innerText = `Updated at : ${getDate()}`;
-        console.log(todoList);
-        // !
+        // ! if the card's content empty
         if (content.innerText == 0) {
           containerOfContent.className = "break-all flex-auto h-7";
         }
@@ -137,53 +189,4 @@ document.getElementById("add").addEventListener("click", () => {
     containerOfSvgs
   );
   // ! end
-});
-
-// ! gets input's value and returns it
-function inputValueRecipient() {
-  return document.getElementsByTagName("input").item(0).value;
-}
-
-// ! gets date of current time
-function getDate() {
-  let d = new Date();
-  return `${
-    d.getMonth() + 1
-  }.${d.getDate()}.${d.getFullYear()} / ${d.getSeconds()} : ${d.getMinutes()} : ${d.getHours()}`;
-}
-
-// ! copies the svg
-function svgCopier(editor1, remover1) {
-  const editor = editor1.cloneNode(true);
-  const remover = remover1.cloneNode(true);
-  remover.setAttribute("class", "recycle_bin-primary");
-  editor.setAttribute("class", "editor-primary");
-  return {
-    editor,
-    remover,
-  };
-}
-
-// ! will get the data when site loaded
-function gettingInfo() {
-  // let tempArr = [];
-  // try {
-  //   tempArr = JSON.parse(localStorage.getItem("todo"));
-  //   console.log(tempArr);
-  // } catch (error) {
-  //   console.log(error);
-  // }
-}
-
-// ! will be something before user leave the site
-function savingInfo() {}
-
-// ! adds information into the global array (todoList)
-function addTodo(id, todo, createdAt, updatedAt) {
-  todoList.push({
-    id,
-    createdAt,
-    todo,
-    updatedAt,
-  });
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", "./**/*.{html,js}", "./index.html"],
+  content: ["./src/**/*.{html,js}", "./**/*.{html,js}"],
   theme: {
     extend: {
       borderRadius: {
