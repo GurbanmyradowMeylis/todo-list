c2cfce613917b5872dd6a6d5fd260eb02c01f002
SuperACER <gurbanmyradowmeylis280@gmail.com>
2023-08-20 15:59:54 +0500

    did the localstorage

--- js/app.js
+++ js/app.js
@@ -5,6 +5,18 @@ document.getElementById("add").addEventListener("click", () => {
   adderTodoList(identifier, inputValueRecipient(), getDate(), getDate(), true);
 });
 
+document.getElementById("input-content").addEventListener("keydown", (ev) => {
+  if (ev.key.toLowerCase() === "enter") {
+    adderTodoList(
+      identifier,
+      inputValueRecipient(),
+      getDate(),
+      getDate(),
+      true
+    );
+  }
+});
+
 // ! gets input's value and returns it
 function inputValueRecipient() {
   return document.getElementsByTagName("input").item(0).value;
@@ -32,10 +44,11 @@ function svgCopier(editor1, remover1) {
 
 // ! will get the data when site loaded
 function gettingInfo() {
-  if (localStorage.length != 0) {
-    todoList = JSON.parse(localStorage.getItem("todo"));
+  let tempArr = JSON.parse(localStorage.getItem("todo"));
+  if (tempArr.length != 0) {
+    todoList = tempArr;
     todoList.forEach((item) => {
-      adderTodoList(item.id, item.todo, item.createdAt, item.updatedAt);
+      adderTodoList(item.id, item.todo, item.createdAt, item.updatedAt, false);
     });
   }
 }
@@ -73,8 +86,8 @@ function adderTodoList(
     createdAt = document.createElement("p"),
     updatedAt = document.createElement("p"),
     containerOfSvgs = document.createElement("div"),
-    containerOfRemover = document.createElement("div"),
-    containerOfEditor = document.createElement("div"),
+    containerOfRemover = document.createElement("a"),
+    containerOfEditor = document.createElement("a"),
     remover = document.getElementById("remover"),
     editor = document.getElementById("editor");
   // ! end
@@ -88,18 +101,30 @@ function adderTodoList(
   id.className = "card__id";
   // ! end
 
-  // ! sets number card's id
-  id.innerText = `Id : ${cardId}`;
-
   // ! increments the identifier
   identifier++;
-
+  let idForInput;
   // ! sets text to content. text --that in input
   content.innerText = cardContent;
-
   if (isAdd) {
-    // ! pushes info from card into the array
-    addTodo(identifier, content.innerText, getDate(), getDate());
+    if (todoList.length == 0) {
+      // ! pushes info from card into the array
+      id.innerText = `Id : ${cardId}`;
+      addTodo(cardId, content.innerText, getDate(), getDate());
+      idForInput = cardId;
+    } else {
+      idForInput = todoList[todoList.length - 1].id + 1;
+      id.innerText = `Id : ${todoList[todoList.length - 1].id + 1}`;
+      addTodo(
+        todoList[todoList.length - 1].id + 1,
+        content.innerText,
+        getDate(),
+        getDate()
+      );
+    }
+  } else {
+    id.innerText = `Id : ${cardId}`;
+    idForInput = cardId;
   }
 
   // ! if the card empty
@@ -108,7 +133,6 @@ function adderTodoList(
   } else {
     containerOfContent.className = "break-all flex-auto font-normal";
   }
-
   // ! gets date and sets into these element
   createdAt.innerText = `Created at : ${cardCreatedAt}`;
   updatedAt.innerText = `Updated at : ${cardUpdatedAt}`;
@@ -124,6 +148,10 @@ function adderTodoList(
       );
     }
   });
+
+  let fullInputId = `iasqgqedwwdsd-1fbhiewbufbcqalruvcbjqw3ui-nbuv29hd12bmn${idForInput}uoqbfljqenI30EGDBGNBWKEH`;
+  containerOfEditor.href = `#${fullInputId}`;
+
   // ! appends childs into the parent
   let clonedEditor = svgCopier(editor, remover).editor;
   containerOfId.append(id);
@@ -142,26 +170,52 @@ function adderTodoList(
       button.innerText = "edit";
       button.className = "button-primary ml-1 w-[20%]";
       input.className = "input w-[75%] rounded-md";
+      input.type = "text";
+      input.id = fullInputId;
       containerOfEdit.id = "editContainer";
       input.value = content.innerText;
       active = !active;
       content.style.display = "none";
       containerOfEdit.append(input, button);
+      console.log(document.getElementById(fullInputId));
       containerOfContent.append(containerOfEdit);
+      input.addEventListener("keydown", (ev) => {
+        if (ev.key.toLowerCase() == "enter") {
+          content.style.display = "block";
+          content.innerText = input.value;
+          containerOfEdit.remove();
+          active = !active;
+          let currentId = id.innerText.split(": ")[1];
+          todoList.splice(
+            todoList.findIndex((item) => item.id == currentId),
+            1,
+            {
+              id: Number(currentId),
+              todo: content.innerText,
+              createdAt: createdAt.innerText.slice(12),
+              updatedAt: getDate(),
+            }
+          );
+          updatedAt.innerText = `Updated at : ${getDate()}`;
+          // ! if the card's content empty
+          if (content.innerText == 0) {
+            containerOfContent.className = "break-all flex-auto h-7";
+          }
+        }
+      });
       button.addEventListener("click", () => {
         content.style.display = "block";
         content.innerText = input.value;
         containerOfEdit.remove();
         active = !active;
-
         let currentId = id.innerText.split(": ")[1];
         todoList.splice(
-          todoList.findIndex((item, i) => item.id == currentId),
+          todoList.findIndex((item) => item.id == currentId),
           1,
           {
             id: Number(currentId),
             todo: content.innerText,
-            createdAt: createdAt.innerText,
+            createdAt: createdAt.innerText.slice(12),
             updatedAt: getDate(),
           }
         );
@@ -172,9 +226,13 @@ function adderTodoList(
         }
       });
     } else {
-      document.getElementById("editContainer").remove();
-      content.style.display = "block";
-      active = !active;
+      document.getElementById(fullInputId).addEventListener("keydown", (ev) => {
+        if (ev.key.toLowerCase() == "enter") {
+          document.getElementById("editContainer").remove();
+          content.style.display = "block";
+          active = !active;
+        }
+      });
     }
   });
 
@@ -190,3 +248,20 @@ function adderTodoList(
   );
   // ! end
 }
+
+function clearAll() {
+  if (confirm("Are you sure to remove all the cards")) {
+    document.getElementById("wrapperOfCard").remove();
+    localStorage.clear();
+    todoList = [];
+    let parentCard = document.createElement("div");
+    parentCard.className = "cards__parent-cards";
+    parentCard.id = "wrapperOfCard";
+    document.getElementById("cards").append(parentCard);
+    identifier = 1;
+  }
+}
+
+document.getElementById("removerAll").addEventListener("click", () => {
+  clearAll();
+});
